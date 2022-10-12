479f0f7c38a69f05b7b1863a3afb7bdc42624d8b
mhtaba <mohammad.tabatabaei@kreftregisteret.no>
2022-10-12 13:16:36 +0000

    consent reply

--- pyprocessor/smartmed_tp.py
+++ pyprocessor/smartmed_tp.py
@@ -199,13 +199,13 @@ class smartmedTransactionHandler(TransactionHandler):
              = state_entries[0].data.decode().split(',')
         LOGGER.info("project issuer = %s.", project_issuer.replace("'","").strip())    
         if username == project_issuer.replace("'","").strip():
-            consent_reply = []
+            consent_reply = {}
             fr = open("/home/azure-dlt-node1/CRN-demo/smartmed-cms/pyprocessor/dslist.txt","r")
             lines = fr.readlines()
             for line in lines:
                 data = line.strip().split(",")
                 if data[1].casefold() == DS_selection_criteria.replace("'","").strip():
-                    consent_reply.append(data[1])
+                    consent_reply[data[0]] = "no response"
             fr.close()        
             query_result = projectID,feasibility,ethicality,approved_time,validity_duration,legal_base, \
                 DS_selection_criteria,project_issuer,HD_transfer_proof,consent_reply
